71ac33ddc02260aa675559a51e41b167a67c8aba
Monil-007 <gandhi.monil007@gmail.com>
2026-03-03 20:40:41 +0530

    Initial commit : Digital Lending App setup with spring boot 4.0.3 and java verison 17 base configurations

---
+++ src/test/java/com/example/digitalLendingApp/DigitalLendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalLendingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
